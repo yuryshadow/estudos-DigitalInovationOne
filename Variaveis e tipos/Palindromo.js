b52ac5cc9e7e9261d3d0c3926df9b6b228c003bf
Yuri <yuryshadow@gmail.com>
2021-10-30 13:56:44 -0300

    Exercicio arrayPares

--- Variaveis e tipos/Palindromo.js
+++ Variaveis e tipos/Palindromo.js
@@ -11,14 +11,16 @@
 //Solução 2
 // omo
 
-function verificaPalindromo2(String){
+function verificaPalindromo2(string){
     if(!string) return "string inexistente";
 
     for(let i = 0; i < string.length / 2; i++){
-        if(string[i] !== string.length -1 -i){
+        if(string[i] !== string[string.length -1 -i]){
         return false;
         }
     }
 
     return true;
 }
+
+console.log(verificaPalindromo2("abba"));
